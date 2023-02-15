b6f1af1b732ac1a94174f92714ff6ec0a5141681
Joshua Valdez <josh@augnos.com>
2023-02-14 17:02:18 -0800

    Updated file

--- COMP250/Assignment05_RecursiveFunctions/recFunctions.cpp
+++ COMP250/Assignment05_RecursiveFunctions/recFunctions.cpp
@@ -4,10 +4,6 @@
 #include <string>
 using namespace std;
 
-/*      Part 1: Reverse String
-Write a recursive function named recurseReverse which prints out a string in reverse using recursion.
-To work properly the function will need to take parameters for the string and a start index (int).
-*/
 
 string recurseReverse(string input, string output = "")
 {
@@ -24,16 +20,6 @@ string recurseReverse(string input, string output = "")
     // recursion
     return recurseReverse(input, output);
 }
-
-/*      Part 2: Max Digit
-Write recursive function called maxDigit which returns the largest digit in a number.
-Your function should take in an int parameter input.
-The base case should be for any input less than 10 simply return the digit.
-You need to save the current last digit. (Use % 10)
-Then, to make the problem smaller: (recursive case)
-You can get rid of the smallest (least significant digit) input by dividing an integer by 10.
-Compare the current last digit to the result of the recursive call on smaller int value.
-*/
 
 int maxDigit(int input, int max = 0)
 {
@@ -58,11 +44,7 @@ int maxDigit(int input, int max = 0)
 
 int main()
 {
-    /* ========== Part 1 ==========
-    In the main function: Test your function on a string at least 6 characters long.
-    Print out the original string and then call your function to show its output.
-    */
-    
+    // Part 1
     string input;
     cout << "Part 1: Reverse String\n" << "Input: ";
     cin >> input;
@@ -70,17 +52,15 @@ int main()
     cout << "Output:" << endl;
     cout << recurseReverse(input) << "\n\n";
 
-    /* ========== Part 2 ==========
-    In the main function: Test your function on a number at least 4 digits long.
-    Print out the original value and then call your function and show its output.
-    */
 
+    // Part 2
     int number;
     cout << "Part 2: Max Digit\n" << "Input: ";
     cin >> number;
 
     cout << "Output:" << endl;
     cout << maxDigit(number) << "\n\n";
+
 
     return 0;
 }
