d85ad1dcd6207f9663186bc0563ebb38d984c3ba
Joshua Valdez <josh@augnos.com>
2023-04-17 02:17:13 -0700

    More work on SkyMart

--- COMP250/2023.03.20_SkyMart/Product.cpp
+++ COMP250/2023.03.20_SkyMart/Product.cpp
@@ -1,75 +1,164 @@
-#include <iostream>
 #include "Product.h"
-
-using namespace std;
+#include <iostream>
+#include <string>
 
 // ========== Product Functions ==========
 // +++++ Product Constructors +++++
-Product::Product(string name) {
+Product::Product(string name)
+{
     this->name = name;
 }
-Product::Product(string name, float price) {
+Product::Product(string name, float price)
+{
     this->name = name;
     this->price = price;
 }
 
 // +++++ Product Accessors +++++
-string Product::getName(){
+string Product::getName() const
+{
     return name;
 }
-float Product::getPrice(){
+float Product::getPrice() const
+{
     return price;
 }
 
 // +++++ Product Mutators +++++
-void Product::setName(string name){
+void Product::setName(string name)
+{
     this->name = name;
 }
-void Product::setPrice(float price){
+void Product::setPrice(float price)
+{
     this->price = price;
 }
 
 // +++++ Product Functions +++++
-void Product::print(){
-    cout << "Product: " << name << " ($" << price << ")\n";
+void Product::print()
+{
+    std::cout << "Product: " << name << " ($" << price << ")\n";
 }
-
 
 // ========== FoodProduct Functions ==========
 // +++++ FoodProduct Constructors +++++
-FoodProduct::FoodProduct(string name, int expirationDate) : Product(name) {
+FoodProduct::FoodProduct(string name, int expirationDate) : Product(name)
+{
     this->expirationDate = expirationDate;
     type = "Unknown";
 }
-FoodProduct::FoodProduct(string name, float price, int expirationDate) : Product(name, price) {
+FoodProduct::FoodProduct(string name, float price, int expirationDate) : Product(name, price)
+{
     this->expirationDate = expirationDate;
     type = "Unknown";
 }
 
 // +++++ FoodProduct Accessors +++++
-string FoodProduct::getType(){
+string FoodProduct::getType() const
+{
     return type;
 }
-int FoodProduct::getExpirationDate(){
+int FoodProduct::getExpirationDate() const
+{
     return expirationDate;
 }
 
 // +++++ FoodProduct Mutators +++++
-void FoodProduct::setType(string type){
+void FoodProduct::setType(string type)
+{
     this->type = type;
 }
-void FoodProduct::setExpirationDate(int expirationDate){
+void FoodProduct::setExpirationDate(int expirationDate)
+{
     this->expirationDate = expirationDate;
 }
 
 // +++++ FoodProduct Functions +++++
-void FoodProduct::print(){
+void FoodProduct::print()
+{
     Product::print();
-    cout << "Food type: " << type << endl;
+    std::cout << "Food type: " << type << std::endl;
+}
+
+// ========== Cart Functions ==========
+// +++++ Cart Constructors +++++
+Cart::Cart()
+{
+    for (int i = 0; i < 99; i++)
+    {
+        prodList[i] = nullptr;
+    }
+    count = 0;
+}
+
+// +++++ Cart Accessors +++++
+int Cart::getProductCount()
+{
+    return count;
+}
+float Cart::getTotal()
+{
+    float total = 0;
+    for (int i = 0; i < count; i++)
+    {
+        total += prodList[i]->getPrice();
+    }
+    return total;
+}
+
+// +++++ Cart Mutators +++++
+bool Cart::addProduct(Product *prodPtr)
+{
+    if (count < 99)
+    {
+        prodList[count] = prodPtr;
+        count++;
+        return true;
+    }
+    return false;
+}
+bool Cart::removeProduct(Product *prodPtr ){
+    int index = findProduct(prodPtr);
+    if (index == -1)
+    {
+        return false;
+    }
+    for (int j = index + 1; j < count; j++)
+    {
+        prodList[index] = prodList[j];
+        index++;
+    }
+    count--;
+    return true;
+}
+
+// +++++ Cart Functions +++++
+void Cart::print()
+{
+    for (int i = 0; i < count; i++)
+    {
+        prodList[i]->print();
+    }
+    std::cout << "Total: $" << getTotal();
+}
+int Cart::findProduct(Product *prodPtr){
+    for (int i = 0; i < count; i++)
+    {
+        if (*prodList[i] == *prodPtr)
+            return i;
+    }
+    return -1;
+}
+
+// +++++ Operator Overloads +++++
+bool Product::operator==(const Product &anotherProduct) const
+{
+    return (name == anotherProduct.getName()) ? true : false;
 }
 
 
-int main (){
+int main()
+{
     Product soapObj("Dove Hand Wash", 4.69);
     soapObj.print();
 
@@ -77,11 +166,19 @@ int main (){
     foodObj.print();
 
     Product inventory[2] = {soapObj, foodObj};
-    cout << endl << "Inventory printout\n";
-    for (int i = 0; i < 2; i++){
+    std::cout << "Inventory printout\n";
+    for (int i = 0; i < 2; i++)
+    {
         inventory[i].print();
     }
 
+    Product *productPtr[2];
+    productPtr[0] = &soapObj;
+    productPtr[1] = &foodObj;
+    for (int i = 0; i < 2; i++)
+    {
+        productPtr[i]->print();
+    }
 
     return 0;
 }
--- COMP250/2023.03.20_SkyMart/Product.h
+++ COMP250/2023.03.20_SkyMart/Product.h
@@ -1,30 +1,50 @@
 #include <iostream>
 using std::string;
 
-class Product {
-private:
+class Product
+{
+  private:
     string name;
     float price;
-public:
+
+  public:
     Product(string);
     Product(string, float);
-    string getName();
-    float getPrice();
+    string getName() const;
+    float getPrice() const;
     void setName(string);
     void setPrice(float);
+    virtual void print();
+    bool operator==(const Product &) const;
+};
+
+class FoodProduct : public Product
+{
+  private:
+    string type;        // category of food, e.g. Dairy, Fruit, etc.
+    int expirationDate; // formatted YYYYMMDD
+  public:
+    FoodProduct(string, int);
+    FoodProduct(string, float, int);
+    string getType() const;
+    int getExpirationDate() const;
+    void setType(string);
+    void setExpirationDate(int);
     void print();
 };
 
-class FoodProduct : public Product{
-private:
-    string type;    // category of food, e.g. Dairy, Fruit, etc.
-    int expirationDate; // formatted YYYYMMDD
-public:
-    FoodProduct(string, int);
-    FoodProduct(string, float, int);
-    string getType();
-    int getExpirationDate();
-    void setType(string);
-    void setExpirationDate(int);
+class Cart
+{
+  private:
+    Product *prodList[99];
+    int count;
+
+  public:
+    Cart();
+    bool addProduct(Product *);
+    bool removeProduct(Product *);
+    int getProductCount();
+    float getTotal();
+    int findProduct(Product *);
     void print();
 };
